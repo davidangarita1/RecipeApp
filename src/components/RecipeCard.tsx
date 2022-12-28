a1885de8a91aa121cc98adb0514d0079ac476563
David Angarita <davidangarita.sistemas@gmail.com>
2022-12-27 23:54:00 -0500

    Se Agrega avance de tarjetas de recetas

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,15 +1,35 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
+import portion from "../assets/icons/ic_portion.svg";
+import time from "../assets/icons/ic_time.svg";
 
 type RecipeCardProps = {
   recipe: any;
 };
 
 const RecipeCard = ({ recipe }: RecipeCardProps): JSX.Element => {
+  const [active, setActive] = useState(false);
+
   return (
     <Fragment>
-      <div className="recipeCard">
+      <div
+        className={`recipeCard ${active ? "show" : ""}`}
+        onMouseOver={() => setActive(true)}
+        onMouseOut={() => setActive(false)}
+      >
         <img src={recipe.thumbnail_url} className="img" alt={recipe.name} />
-        <div className="title">{recipe.name}</div>
+        {active ? (
+          <div className="menu">
+            <img src={portion} alt="" />
+            <span className="subTitle">Tamaño de la porción</span>
+            <div>{recipe.num_servings} raciones</div>
+            <img src={time} alt="" />
+            <span className="subTitle">Tiempo de preparación</span>
+            <div>{recipe.total_time_minutes} minutos</div>
+          </div>
+        ) : (
+          <div className="title">{recipe.name}</div>
+        )}
       </div>
     </Fragment>
   );
