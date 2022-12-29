28be77f88a5ec29fe81ef8d9b6bfea41360ad2fd
David Angarita <davidangarita.sistemas@gmail.com>
2022-12-28 20:31:25 -0500

    Se terminana tarjetas de recetas

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -2,6 +2,9 @@ import { Fragment, useState } from "react";
 
 import portion from "../assets/icons/ic_portion.svg";
 import time from "../assets/icons/ic_time.svg";
+import difficultyImg from "../assets/icons/ic_chef.svg";
+import star from "../assets/icons/ic_star.svg";
+import favorite from "../assets/icons/ic-favorite.svg";
 
 type RecipeCardProps = {
   recipe: any;
@@ -10,10 +13,21 @@ type RecipeCardProps = {
 const RecipeCard = ({ recipe }: RecipeCardProps): JSX.Element => {
   const [active, setActive] = useState(false);
 
+  const difficultyList: string[] = ["Fácil", "Normal", "Difícil"];
+  const score: string = (Math.trunc(Math.random() * 5) + 1).toFixed(1);
+  const difficulty: string = difficultyList[Math.trunc(Math.random() * 3)];
+
+  const getShortName = (name: string): JSX.Element => (
+    <>
+      <span className="firstText">{name.split(" ")[0]} </span>
+      <span className="secondText">{`${name.split(" ")[1]}`}</span>
+    </>
+  );
+
   return (
     <Fragment>
       <div
-        className={`recipeCard ${active ? "show" : ""}`}
+        className={`recipeCard`}
         onMouseOver={() => setActive(true)}
         onMouseOut={() => setActive(false)}
       >
@@ -22,13 +36,23 @@ const RecipeCard = ({ recipe }: RecipeCardProps): JSX.Element => {
           <div className="menu">
             <img src={portion} alt="" />
             <span className="subTitle">Tamaño de la porción</span>
-            <div>{recipe.num_servings} raciones</div>
+            <span className="text">{recipe.num_servings} raciones</span>
             <img src={time} alt="" />
             <span className="subTitle">Tiempo de preparación</span>
-            <div>{recipe.total_time_minutes} minutos</div>
+            <span className="text">{recipe.total_time_minutes} minutos</span>
+            <img src={difficultyImg} alt="" />
+            <span className="subTitle">Dificultad</span>
+            <span className="text">{difficulty}</span>
           </div>
         ) : (
-          <div className="title">{recipe.name}</div>
+          <>
+            <div className="cardTitle">{getShortName(recipe.name)}</div>
+            <div className="score">
+              <img className="star" src={star} alt="" />
+              <span>{score}</span>
+              <img className="heart" src={favorite} alt="" />
+            </div>
+          </>
         )}
       </div>
     </Fragment>
